9c77fdf507dd6cb8f51c9caeec5ebd9fb2e33bcc
Solana-Swap <support@solanaswap.co>
2024-10-24 15:49:44 +0800

    Add files via upload

---
+++ static/token.js
@@ -0,0 +1,14 @@
+window.TOKENLIST = [
+  {
+    icon: "/static/coins/s.png",
+    name: "SOL",
+    symbol: "SOL",
+    token: "So11111111111111111111111111111111111111112",
+  },
+  {
+    icon: "/static/coins/usdt.png",
+    name: "USDT",
+    symbol: "USDT",
+    token: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+  },
+];
