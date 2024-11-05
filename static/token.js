9d768b508ccf2b0d7ca4d83173b002ca113d0f94
Solana-Swap <support@solanaswap.co>
2024-11-06 01:36:10 +0800

    Add files via upload

--- static/token.js
+++ static/token.js
@@ -1,14 +1,14 @@
 window.TOKENLIST = [
   {
     icon: "/static/coins/s.png",
+    name: "SolanaSwap",
+    symbol: "SOS",
+    token: "HDa3zJc12ahykSsBRvgiWzr6WLEByf36yzKKbVvy4gnF",
+  },
+  {
+    icon: "/static/coins/sol.jpg",
     name: "SOL",
     symbol: "SOL",
     token: "So11111111111111111111111111111111111111112",
-  },
-  {
-    icon: "/static/coins/usdt.png",
-    name: "USDT",
-    symbol: "USDT",
-    token: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
   },
 ];
